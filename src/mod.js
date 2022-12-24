93781e8e3bd8bfd5261171c8b82ae36700bb78e0
JustNU <ivancool108@gmail.com>
2022-12-24 21:33:52 +0300

    add western MRE, adjust pricing and secure container filters

--- src/mod.js
+++ src/mod.js
@@ -23,9 +23,37 @@ class Mod {
 		
 		// replace iskra in database
 		database.templates.items["590c5d4b86f774784e1b9c45"] = modDb.items["590c5d4b86f774784e1b9c45"];
+		database.templates.prices["590c5d4b86f774784e1b9c45"] = 24000
 		
-		// to do
-		// adjust price, add western MRE
+		// replace western MRE
+		database.templates.items["590c5f0d86f77413997acfab"] = modDb.items["590c5f0d86f77413997acfab"];
+		database.templates.prices["590c5f0d86f77413997acfab"] = 27000
+		
+		// adjust prices
+		let iskraChanged = false;
+		let mreChanged = false;
+		for (const itemIndex in database.templates.handbook.Items) {
+			if (iskraChanged && mreChanged) {
+				break;
+			}
+			
+			if (database.templates.handbook.Items[itemIndex].Id === "590c5d4b86f774784e1b9c45")	{
+				database.templates.handbook.Items[itemIndex].Price = 21125;
+				iskraChanged = true;
+			}
+			
+			if (database.templates.handbook.Items[itemIndex].Id === "590c5f0d86f77413997acfab")	{
+				database.templates.handbook.Items[itemIndex].Price = 24832;
+				mreChanged = true;
+			}
+		}
+		
+		//adjust secure container filters
+		for (const item in database.templates.items) {
+			if (database.templates.items[item]._parent === "5448bf274bdc2dfc2f8b456a") {
+				database.templates.items[item]._props.Grids[0]._props.filters[0].Filter.push(...["590c5d4b86f774784e1b9c45", "590c5f0d86f77413997acfab"]);
+			}
+		}
 	}
 	
 	postAkiLoad(container) {
@@ -34,8 +62,11 @@ class Mod {
 		const inventoryConfig = configServer.getConfig("aki-inventory");
 		const modDb = require("../db/items/itemData.json");
 		
-		// add iskra items
+		// add iskra loot
 		inventoryConfig.randomLootContainers["590c5d4b86f774784e1b9c45"] = modDb.randomLootContainers["590c5d4b86f774784e1b9c45"];
+		
+		// add western mre loot
+		inventoryConfig.randomLootContainers["590c5f0d86f77413997acfab"] = modDb.randomLootContainers["590c5f0d86f77413997acfab"];
 	}
 	
 	static customOpenRandomLootContainer(pmcData, body, sessionID) {
